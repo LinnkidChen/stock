a2e28ab73bc5877abf68e3ed2e4c5a98be18a42f
Linnkid_CHEN <56332312+LinnkidChen@users.noreply.github.com>
2023-09-08 23:09:48 +0800

    add logger

--- stock/jobs/18h_daily_job.py
+++ stock/jobs/18h_daily_job.py
@@ -6,6 +6,7 @@ import libs.common as common
 import sys
 import time
 import pandas as pd
+from libs.logger import logger
 import numpy as np
 from sqlalchemy.types import NVARCHAR
 from sqlalchemy import inspect
@@ -13,6 +14,7 @@ import datetime
 import akshare as ak
 
 import MySQLdb
+
 
 # 600开头的股票是上证A股，属于大盘股
 # 600开头的股票是上证A股，属于大盘股，其中6006开头的股票是最早上市的股票，
@@ -25,10 +27,19 @@ def stock_a(code):
     # print(code)
     # print(type(code))
     # 上证A股  # 深证A股
-    if code.startswith('600') or code.startswith('6006') or code.startswith('601') or code.startswith('000') or code.startswith('001') or code.startswith('002'):
+    if (
+        code.startswith("600")
+        or code.startswith("6006")
+        or code.startswith("601")
+        or code.startswith("000")
+        or code.startswith("001")
+        or code.startswith("002")
+    ):
         return True
     else:
         return False
+
+
 # 过滤掉 st 股票。
 def stock_a_filter_st(name):
     # print(code)
@@ -39,6 +50,7 @@ def stock_a_filter_st(name):
     else:
         return False
 
+
 # 过滤价格，如果没有基本上是退市了。
 def stock_a_filter_price(latest_price):
     # float 在 pandas 里面判断 空。
@@ -47,10 +59,10 @@ def stock_a_filter_price(latest_price):
     else:
         return True
 
+
 ####### 3.pdf 方法。宏观经济数据
 # 接口全部有错误。只专注股票数据。
 def stat_all(tmp_datetime):
-
     datetime_str = (tmp_datetime).strftime("%Y-%m-%d")
     datetime_int = (tmp_datetime).strftime("%Y%m%d")
     print("datetime_str:", datetime_str)
@@ -62,29 +74,53 @@ def stat_all(tmp_datetime):
         # print(data.index)
         # 解决ESP 小数问题。
         # data["esp"] = data["esp"].round(2)  # 数据保留2位小数
-        data.columns = ['index', 'code', 'name', 'last_price', 'change_percent', 'change_amount',
-        'volume', 'turnover', 'amplitude', 'high', 'low', 'open', 'closed', 'volume_ratio',
-        'turnover_rate', 'pe_ratio','pb_ratio', 'market_cap','circulating_market_cap','rise_speed',
-        'change_5min', 'change_ercent_60day','ytd_change_percent']
+        data.columns = [
+            "index",
+            "code",
+            "name",
+            "last_price",
+            "change_percent",
+            "change_amount",
+            "volume",
+            "turnover",
+            "amplitude",
+            "high",
+            "low",
+            "open",
+            "closed",
+            "volume_ratio",
+            "turnover_rate",
+            "pe_ratio",
+            "pb_ratio",
+            "market_cap",
+            "circulating_market_cap",
+            "rise_speed",
+            "change_5min",
+            "change_ercent_60day",
+            "ytd_change_percent",
+        ]
 
-        data = data.loc[data["code"].apply(stock_a)].loc[data["name"].apply(stock_a_filter_st)].loc[
-            data["last_price"].apply(stock_a_filter_price)]
+        data = (
+            data.loc[data["code"].apply(stock_a)]
+            .loc[data["name"].apply(stock_a_filter_st)]
+            .loc[data["last_price"].apply(stock_a_filter_price)]
+        )
         print(data)
-        data['date'] = datetime_int  # 修改时间成为int类型。
+        data["date"] = datetime_int  # 修改时间成为int类型。
 
         # 删除老数据。
-        del_sql = " DELETE FROM `stock_zh_a_spot_em` where `date` = '%s' " % datetime_int
+        del_sql = (
+            " DELETE FROM `stock_zh_a_spot_em` where `date` = '%s' " % datetime_int
+        )
         common.insert(del_sql)
 
-        data.set_index('code', inplace=True)
-        data.drop('index', axis=1, inplace=True)
+        data.set_index("code", inplace=True)
+        data.drop("index", axis=1, inplace=True)
         print(data)
         # 删除index，然后和原始数据合并。
         common.insert_db(data, "stock_zh_a_spot_em", True, "`date`,`code`")
     except Exception as e:
         print("error :", e)
-
-
 
     # 龙虎榜-个股上榜统计
     # 接口: stock_lhb_ggtj_sina
@@ -98,26 +134,38 @@ def stat_all(tmp_datetime):
         stock_lhb_ggtj_sina = ak.stock_lhb_ggtj_sina(recent_day="5")
         print(stock_lhb_ggtj_sina)
 
-        stock_lhb_ggtj_sina.columns = ['code', 'name', 'ranking_times', 'sum_buy', 'sum_sell', 'net_amount', 'buy_seat',
-                                       'sell_seat']
+        stock_lhb_ggtj_sina.columns = [
+            "code",
+            "name",
+            "ranking_times",
+            "sum_buy",
+            "sum_sell",
+            "net_amount",
+            "buy_seat",
+            "sell_seat",
+        ]
 
-        stock_lhb_ggtj_sina = stock_lhb_ggtj_sina.loc[stock_lhb_ggtj_sina["code"].apply(stock_a)].loc[
-            stock_lhb_ggtj_sina["name"].apply(stock_a_filter_st)]
+        stock_lhb_ggtj_sina = stock_lhb_ggtj_sina.loc[
+            stock_lhb_ggtj_sina["code"].apply(stock_a)
+        ].loc[stock_lhb_ggtj_sina["name"].apply(stock_a_filter_st)]
 
-        stock_lhb_ggtj_sina.set_index('code', inplace=True)
+        stock_lhb_ggtj_sina.set_index("code", inplace=True)
         # data_sina_lhb.drop('index', axis=1, inplace=True)
         # 删除老数据。
-        stock_lhb_ggtj_sina['date'] = datetime_int  # 修改时间成为int类型。
+        stock_lhb_ggtj_sina["date"] = datetime_int  # 修改时间成为int类型。
 
         # 删除老数据。
-        del_sql = " DELETE FROM `stock_lhb_ggtj_sina` where `date` = '%s' " % datetime_int
+        del_sql = (
+            " DELETE FROM `stock_lhb_ggtj_sina` where `date` = '%s' " % datetime_int
+        )
         common.insert(del_sql)
 
-        common.insert_db(stock_lhb_ggtj_sina, "stock_lhb_ggtj_sina", True, "`date`,`code`")
+        common.insert_db(
+            stock_lhb_ggtj_sina, "stock_lhb_ggtj_sina", True, "`date`,`code`"
+        )
 
     except Exception as e:
         print("error :", e)
-
 
     # 每日统计
     # 接口: stock_dzjy_mrtj
@@ -127,31 +175,46 @@ def stat_all(tmp_datetime):
     # 描述: 获取东方财富网-数据中心-大宗交易-每日统计
 
     try:
-
         print("################ tmp_datetime : " + datetime_str)
 
-        stock_dzjy_mrtj = ak.stock_dzjy_mrtj(start_date=datetime_str, end_date=datetime_str)
+        stock_dzjy_mrtj = ak.stock_dzjy_mrtj(
+            start_date=datetime_str, end_date=datetime_str
+        )
         print(stock_dzjy_mrtj)
 
-        stock_dzjy_mrtj.columns = ['index', 'trade_date', 'code', 'name', 'quote_change', 'close_price', 'average_price',
-                                   'overflow_rate', 'trade_number', 'sum_volume', 'sum_turnover',
-                                   'turnover_market_rate']
+        stock_dzjy_mrtj.columns = [
+            "index",
+            "trade_date",
+            "code",
+            "name",
+            "quote_change",
+            "close_price",
+            "average_price",
+            "overflow_rate",
+            "trade_number",
+            "sum_volume",
+            "sum_turnover",
+            "turnover_market_rate",
+        ]
 
-        stock_dzjy_mrtj.set_index('code', inplace=True)
+        stock_dzjy_mrtj.set_index("code", inplace=True)
         # data_sina_lhb.drop('index', axis=1, inplace=True)
         # 删除老数据。
-        stock_dzjy_mrtj['date'] = datetime_int  # 修改时间成为int类型。
-        stock_dzjy_mrtj.drop('trade_date', axis=1, inplace=True)
-        stock_dzjy_mrtj.drop('index', axis=1, inplace=True)
+        stock_dzjy_mrtj["date"] = datetime_int  # 修改时间成为int类型。
+        stock_dzjy_mrtj.drop("trade_date", axis=1, inplace=True)
+        stock_dzjy_mrtj.drop("index", axis=1, inplace=True)
 
         # 数据保留2位小数
         try:
-            stock_dzjy_mrtj = stock_dzjy_mrtj.loc[stock_dzjy_mrtj["code"].apply(stock_a)].loc[
-                stock_dzjy_mrtj["name"].apply(stock_a_filter_st)]
+            stock_dzjy_mrtj = stock_dzjy_mrtj.loc[
+                stock_dzjy_mrtj["code"].apply(stock_a)
+            ].loc[stock_dzjy_mrtj["name"].apply(stock_a_filter_st)]
 
             stock_dzjy_mrtj["average_price"] = stock_dzjy_mrtj["average_price"].round(2)
             stock_dzjy_mrtj["overflow_rate"] = stock_dzjy_mrtj["overflow_rate"].round(4)
-            stock_dzjy_mrtj["turnover_market_rate"] = stock_dzjy_mrtj["turnover_market_rate"].round(6)
+            stock_dzjy_mrtj["turnover_market_rate"] = stock_dzjy_mrtj[
+                "turnover_market_rate"
+            ].round(6)
         except Exception as e:
             print("round error :", e)
 
@@ -166,8 +229,10 @@ def stat_all(tmp_datetime):
     except Exception as e:
         print("error :", e)
 
+
 # main函数入口
-if __name__ == '__main__':
+if __name__ == "__main__":
     # 执行数据初始化。
     # 使用方法传递。
+    logger.warning("18h_daily_job starts running")
     tmp_datetime = common.run_with_args(stat_all)
--- stock/libs/logger.py
+++ stock/libs/logger.py
@@ -2,12 +2,18 @@ import logging
 from sys import stdout
 
 # Define logger
-logger = logging.getLogger("mylogger")
+logger = logging.getLogger()
 
 logger.setLevel(logging.DEBUG)  # set logger level
 logFormatter = logging.Formatter(
     "%(name)-12s %(asctime)s %(levelname)-8s %(filename)s:%(funcName)s %(message)s"
 )
+
 consoleHandler = logging.StreamHandler(stdout)  # set streamhandler to stdout
 consoleHandler.setFormatter(logFormatter)
+
+fileHandler = logging.FileHandler(filename="/data/logs/" + "logging.log")
+fileHandler.setFormatter(logFormatter)
+logger.addHandler(fileHandler)
 logger.addHandler(consoleHandler)
+logger.info("logger starts running")
